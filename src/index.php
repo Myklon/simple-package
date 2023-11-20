02b601ae757e4dcb7bcb890c95dfd2aea2c8b2d9
Myklon <livroklon@gmail.com>
2023-11-20 16:51:43 +0300

    feat: add library files

---
+++ src/index.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Myklon\SimplePackage;
+
+class Calculator
+{
+    public function sum($number1, $number2)
+    {
+        return $number1 + $number2;
+    }
+}
